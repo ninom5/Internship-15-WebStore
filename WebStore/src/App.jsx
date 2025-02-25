2d29576c71a9a69a0d5af4e063730727fcb2345f
Nino <nmilas00@fesb.hr>
2025-02-25 19:32:24 +0100

    Add not found page

--- WebStore/src/App.jsx
+++ WebStore/src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import AddNewProductPage from "./pages/AddNewProductPage/AddNewProductPage";
--- WebStore/src/pages/NotFoundPage/NotFoundPage.jsx
+++ WebStore/src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom";
+
 const NotFoundPage = () => {
-  return <></>;
+  return (
+    <div className="not-found-content">
+      <h1>404</h1>
+      <p>Nothing to see here. Go back.</p>
+      <Link to="/">
+        <button>Go back</button>
+      </Link>
+    </div>
+  );
 };
 
 export default NotFoundPage;
