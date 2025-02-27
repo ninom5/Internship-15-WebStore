47d0361b0825e56aa96f21ce43b487a78c915a07
Nino <nmilas00@fesb.hr>
2025-02-27 20:42:11 +0100

    Add search container and dropdown menu; add sorting by rating and showing top 20 products

--- WebStore/src/pages/ProductsPage/ProductsPage.jsx
+++ WebStore/src/pages/ProductsPage/ProductsPage.jsx
@@ -1,10 +1,15 @@
 import { useEffect, useState } from "react";
 import LoadingSkeleton from "../../components/LoadingSkeleton/LoadingSkeleton";
 import ProductCard from "../../components/ProductCard/ProductCard";
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
+import { Menu, MenuItem, Button } from "@mui/material";
 
 const ProductsPage = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [element, setElement] = useState(null);
+  const [open, setOpen] = useState(false);
 
   const localProducts = JSON.parse(localStorage.getItem("products")) || [];
 
@@ -15,12 +20,18 @@ const ProductsPage = () => {
         if (!response.ok) throw new Error("Error fetching data");
 
         const data = await response.json();
-        if (!Array.isArray(data) || !Array.isArray(localProducts)) {
+
+        if (!Array.isArray(data) || !Array.isArray(localProducts))
           throw new Error("Data or localProducts is not an array");
-        }
+
         const allProducts = [...data, ...localProducts];
 
-        setProducts(allProducts);
+        const sortedProducts = allProducts.sort(
+          (a, b) => b.rating?.rate - a.rating?.rate
+        );
+        const topProducts = sortedProducts.slice(0, 20);
+
+        setProducts(topProducts);
       } catch (error) {
         console.error("Error fetching data: ", error);
         return;
@@ -32,23 +43,41 @@ const ProductsPage = () => {
     fetchedData();
   }, []);
 
-  const sortedProducts = products.sort(
-    (a, b) => b.rating?.rate - a.rating?.rate
-  );
-  const topProducts = sortedProducts.slice(0, 20);
+  const handleMenuClick = (e) => {
+    setElement(e.currentTarget);
+    setOpen(true);
+  };
 
-  console.log("sorted: ", topProducts);
+  const handleMenuClose = () => {
+    setOpen(false);
+  };
 
   if (loading) {
     return <LoadingSkeleton numberOfProducts={products.length} />;
   }
 
   return (
-    <section className="all-products">
-      {topProducts.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
-    </section>
+    <>
+      <Autocomplete
+        id="free-solo-demo"
+        freeSolo
+        options={products.map((option) => option.title)}
+        renderInput={(params) => <TextField {...params} label="freeSolo" />}
+      />
+      <Button onClick={handleMenuClick}>Filter by category</Button>
+      <Menu anchorEl={element} open={open} onClose={handleMenuClose}>
+        <MenuItem onClick={() => handleMenuClose}>aa</MenuItem>
+        <MenuItem onClick={() => handleMenuClose}>aa</MenuItem>
+        <MenuItem onClick={() => handleMenuClose}>aa</MenuItem>
+        <MenuItem onClick={() => handleMenuClose}>aa</MenuItem>
+        <MenuItem>bb</MenuItem>
+      </Menu>
+      <section className="all-products">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </section>
+    </>
   );
 };
 
