b047ab772b65836f6d88f00307bb4335e9e5b6d1
Nino <nmilas00@fesb.hr>
2025-02-28 13:01:53 +0100

    Add option to filter products by category

--- WebStore/src/pages/ProductsPage/ProductsPage.jsx
+++ WebStore/src/pages/ProductsPage/ProductsPage.jsx
@@ -7,9 +7,11 @@ import { Menu, MenuItem, Button } from "@mui/material";
 
 const ProductsPage = () => {
   const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [element, setElement] = useState(null);
   const [open, setOpen] = useState(false);
+  const [categorySelect, setCategorySelect] = useState("");
 
   const localProducts = JSON.parse(localStorage.getItem("products")) || [];
 
@@ -32,6 +34,7 @@ const ProductsPage = () => {
         const topProducts = sortedProducts.slice(0, 20);
 
         setProducts(topProducts);
+        setFilteredProducts(topProducts);
       } catch (error) {
         console.error("Error fetching data: ", error);
         return;
@@ -43,6 +46,23 @@ const ProductsPage = () => {
     fetchedData();
   }, []);
 
+  useEffect(() => {
+    let updatedProducts = products;
+
+    if (categorySelect !== "all")
+      updatedProducts = products.filter(
+        (product) => product.category === categorySelect
+      );
+
+    // if (searchTerm) {
+    //   updatedProducts = updatedProducts.filter((product) =>
+    //     product.title.toLowerCase().includes(searchTerm.toLowerCase())
+    //   );
+    // }
+
+    setFilteredProducts(updatedProducts);
+  }, [categorySelect /*searchFilter*/, , products]);
+
   const handleMenuClick = (e) => {
     setElement(e.currentTarget);
     setOpen(true);
@@ -52,30 +72,56 @@ const ProductsPage = () => {
     setOpen(false);
   };
 
+  const handleCategorySelect = (categoryOption) => {
+    setCategorySelect(categoryOption);
+    handleMenuClose();
+  };
+
   if (loading) {
     return <LoadingSkeleton numberOfProducts={products.length} />;
   }
 
   return (
     <>
-      <Autocomplete
-        id="free-solo-demo"
-        freeSolo
-        options={products.map((option) => option.title)}
-        renderInput={(params) => <TextField {...params} label="freeSolo" />}
-      />
-      <Button onClick={handleMenuClick}>Filter by category</Button>
-      <Menu anchorEl={element} open={open} onClose={handleMenuClose}>
-        <MenuItem onClick={() => handleMenuClose}>aa</MenuItem>
-        <MenuItem onClick={() => handleMenuClose}>aa</MenuItem>
-        <MenuItem onClick={() => handleMenuClose}>aa</MenuItem>
-        <MenuItem onClick={() => handleMenuClose}>aa</MenuItem>
-        <MenuItem>bb</MenuItem>
-      </Menu>
+      <h1>Web store</h1>
+      <div className="filters">
+        <Autocomplete
+          id="free-solo-demo"
+          freeSolo
+          options={products.map((option) => option.title)}
+          renderInput={(params) => (
+            <TextField {...params} label="Search by name" />
+          )}
+        />
+
+        <Button onClick={handleMenuClick}>Filter by category</Button>
+
+        <Menu anchorEl={element} open={open} onClose={handleMenuClose}>
+          {["all", "electronics", "men's clothing", "women's clothing"].map(
+            (category) => (
+              <MenuItem
+                key={category}
+                onClick={() => handleCategorySelect(category)}
+              >
+                {category === "all" ? "Top 20 products" : category}
+              </MenuItem>
+            )
+          )}
+        </Menu>
+        <h2>
+          {categorySelect === "all"
+            ? "Top 20 products by rating"
+            : categorySelect}
+        </h2>
+      </div>
       <section className="all-products">
-        {products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
+        {filteredProducts.length > 0
+          ? filteredProducts.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))
+          : products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
       </section>
     </>
   );
